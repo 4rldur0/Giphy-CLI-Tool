0241e715f1b56369dc15436dbfca6096173b0af2
4rldur0 <ngim24863@gmail.com>
2024-03-23 23:55:27 +0900

    Getting Started ~6

---
+++ unit_test.py
@@ -0,0 +1,18 @@
+from data import Data
+import unittest
+
+class DataTest(unittest.TestCase):
+    def test_url(self):
+        api_key = "oZhufry2NyjOWfmCItORiADf8lPGTN2j"
+        params = {
+            "api_key": api_key,
+            "limit": 25,
+            "offset": 0,
+            "rating": "g",
+            "bundle": "messaging_non_clips"
+        }
+        d = Data("trending", api_key, params)
+        self.assertTrue(d.get_data != None, "Data is not invoked")
+
+if __name__ == "__main__":
+    unittest.main()
