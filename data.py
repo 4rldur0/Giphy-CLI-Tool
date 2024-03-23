6d31eacb26eef34afe65c559aba2ab36dd64c357
4rldur0 <ngim24863@gmail.com>
2024-03-23 23:55:27 +0900

    Getting Started ~6

---
+++ data.py
@@ -0,0 +1,13 @@
+import requests
+
+class Data:
+    def __init__(self, endpoint, api_key, params):
+        url = "https://api.giphy.com/v1/gifs"
+        self.url = "/".join([url, endpoint])
+        self.params = params
+        self.data = None
+
+    def get_data(self):
+        data_json = requests.get(self.url, params=self.params)
+        self.data = data_json.json()
+        return self.data
