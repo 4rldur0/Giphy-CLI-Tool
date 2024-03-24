b78c3391e3ca6106cb0486bb6de29c5219d17c38
4rldur0 <ngim24863@gmail.com>
2024-03-24 12:37:11 +0900

    GiphyAPI and GiphyCLI with api test only

---
+++ api.py
@@ -0,0 +1,24 @@
+from data import Data
+import os
+
+class GiphyAPI:
+    def __init__(self):
+        self.params = {
+            "api_key": os.environ["GIPHY_API_KEY"],
+            "limit": 25,
+            "offset": 0,
+            "rating": "g",
+            "bundle": "messaging_non_clips"
+        }
+
+    def trending(self):
+        print("trending subcommand called!")
+        d = Data("trending", self.params)
+        return d.get_data()
+
+    def search(self, q):
+        print("search subcommand called!")
+        self.params['q'] = q
+        d = Data("search", self.params)
+        return d.get_data()
+
