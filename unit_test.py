01b13435f2e9fe345bd2168b3db45fb948fc3e6f
4rldur0 <ngim24863@gmail.com>
2024-03-24 12:47:02 +0900

    small test and medium test

--- unit_test.py
+++ unit_test.py
@@ -3,19 +3,20 @@ from api import GiphyAPI
 import unittest
 
 class Test(unittest.TestCase):
-    api_key = "oZhufry2NyjOWfmCItORiADf8lPGTN2j"
-    params = {
-        "api_key": api_key,
-        "limit": 25,
-        "offset": 0,
-        "rating": "g",
-        "bundle": "messaging_non_clips"
-    }
-
+    # small tests
     def test_url(self):
+        api_key = "oZhufry2NyjOWfmCItORiADf8lPGTN2j"
+        params = {
+            "api_key": api_key,
+            "limit": 25,
+            "offset": 0,
+            "rating": "g",
+            "bundle": "messaging_non_clips"
+        }
         d = Data("trending", self.params)
         self.assertTrue(d.get_data()['meta']['msg'] == 'OK', "responded message should be 'OK'")
 
+    # medium tests
     def test_trending_api(self):
         ga = GiphyAPI()
         self.assertTrue(ga.trending()['meta']['msg'] == 'OK', "responded message should be 'OK'")
