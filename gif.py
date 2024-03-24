96c52e2ad76a59f4cd0febb7309aad8b2b6a652a
4rldur0 <ngim24863@gmail.com>
2024-03-24 10:42:38 +0900

    GiphyAPI class without test

--- gif.py
+++ gif.py
@@ -3,44 +3,43 @@ import click
 import sys
 import os
 
-api_key = os.environ["GIPHY_API_KEY"]
-
-@click.group()
-def gif():
-    print("hello from giphy cli!")
-
-
-@gif.command()
-@click.option('-s', is_flag=True)
-def trending(s):
-    print("trending subcommand called!")
+class GiphyAPI:
     params = {
-        "api_key": api_key,
+        "api_key": os.environ["GIPHY_API_KEY"],
         "limit": 25,
         "offset": 0,
         "rating": "g",
         "bundle": "messaging_non_clips"
     }
-    d = Data("trending", api_key, params)
-    if s:
-        click.echo(d.get_data())
-    return d.get_data()
 
-@gif.command()
-@click.argument('q')
-def search(q):
-    print("search subcommand called!")
-    params = {
-        "api_key": api_key,
-        "q": q,
-        "limit": 25,
-        "offset": 0,
-        "rating": "g",
-        "bundle": "messaging_non_clips"
-    }
-    d = Data("search", api_key, params)
-    return d.get_data()
+    @click.group()
+    @staticmethod
+    def gif():
+        print("hello from giphy cli!")
+
+
+    @gif.command()
+    @click.option('-s', is_flag=True)
+    def trending(s):
+        print("trending subcommand called!")
+        d = Data("trending", GiphyAPI.params)
+        data = d.get_data()
+        if s:
+            click.echo(data)
+        return data
+
+    @gif.command()
+    @click.argument('q')
+    @click.option('-s', is_flag=True)
+    def search(q, s):
+        print("search subcommand called!")
+        self.params['q'] = q
+        d = Data("search", GiphyAPI.params)
+        data = d.get_data()
+        if s:
+            click.echo(data)
+        return data
     
 if __name__ == "__main__":
-    print(api_key)
-    gif()
+    g = GiphyAPI()
+    g.gif()
