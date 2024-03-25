f57dae44d9a7767c60f5babe4f99d2a2648944b9
4rldur0 <ngim24863@gmail.com>
2024-03-26 07:15:38 +0900

    done cli without testing

--- click_test.py
+++ click_test.py
@@ -14,8 +14,20 @@ def test_trending_cli():
     result = runner.invoke(gCLI.trending, ['-s'])
     assert not result.exception
     assert "'msg: 'OK'" in result.output
+
+    result = runner.invoke(gCLI.search, ['-l', 1, '-s'])
+    assert not result.exception
+    assert "'count': 1" in result.output
     
 def test_serach_cli_argument():
     result = runner.invoke(gCLI.search, ['hi'])
     assert not result.exception
     assert "search subcommand called!\n" in result.output
+
+    result = runner.invoke(gCLI.search, ['hi', '-s'])
+    assert not result.exception
+    assert "'msg: 'OK'" in result.output
+
+    result = runner.invoke(gCLI.search, ['hi', '-l', 1, '-s'])
+    assert not result.exception
+    assert "'count': 1" in result.output
--- unit_test.py
+++ unit_test.py
@@ -8,21 +8,36 @@ class Test(unittest.TestCase):
     # small tests
     def test_url(self):
         self.assertTrue(
-            gAPI.get_data("trending")["meta"]["msg"] == "OK", "responded message should be 'OK'"
+            gAPI.get_data("trending")["meta"]["msg"] == "OK", "Responded message should be 'OK'"
         )
 
     # medium tests
     def test_trending_api(self):
+        gAPI.trending()
         self.assertTrue(
-            gAPI.trending()["meta"]["msg"] == "OK", "responded message should be 'OK'"
+            gAPI.data["meta"]["msg"]  == "OK", "Responded message should be 'OK'"
         )
-
+        gAPI.trending(1)
+        self.assertTrue(
+            gAPI.data["meta"]["msg"]  == "OK", "Responded message should be 'OK'"
+        )
+        self.assertTrue(
+            gAPI.data['pagination']['count']  == 1, "There should be 1 item"
+        )
+        
     def test_search_api(self):
         q = "hi"
+        gAPI.search(q)
         self.assertTrue(
-            gAPI.search(q)["meta"]["msg"] == "OK", "responded message should be 'OK'"
+            gAPI.data["meta"]["msg"]  == "OK", "Responded message should be 'OK'"
         )
-
-
+        gAPI.search(q, 1)
+        self.assertTrue(
+            gAPI.data["meta"]["msg"]  == "OK", "Responded message should be 'OK'"
+        )
+        self.assertTrue(
+            gAPI.data['pagination']['count']  == 1, "There should be 1 item"
+        )
+        
 if __name__ == "__main__":
     unittest.main()
